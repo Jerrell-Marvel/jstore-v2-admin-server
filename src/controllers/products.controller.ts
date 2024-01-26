3b583926bb639aed9f9b1a2d72e5c3db32ed51cc
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-01-26 23:39:48 +0700

    add validation for quantity and price

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import { BadRequestError } from "../errors/BadRequestError";
 import { addProduct } from "../services/products.service";
+import { StatusCodes } from "http-status-codes";
 
 export const createProduct = async (req: Request, res: Response) => {
   const { name, description, quantity, price } = req.body;
 
+  const parsedQuantity = parseInt(quantity);
+  const parsedPrice = parseInt(price);
+
   if (!name || !description || !quantity || !price) {
     throw new BadRequestError("missing fields");
+  }
+
+  if (isNaN(parsedQuantity) || isNaN(parsedPrice) || parsedQuantity < 0 || parsedQuantity < 0) {
+    throw new BadRequestError("invalid quantity or price field");
   }
 
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
@@ -17,7 +25,7 @@ export const createProduct = async (req: Request, res: Response) => {
 
   const result = await addProduct({ name, description, quantity, price }, files.displayImage[0], files.productImages);
 
-  return res.json(result);
+  return res.status(StatusCodes.CREATED).json(result);
 };
 
 export const createProductWithVariants = (req: Request, res: Response) => {
