62b9283caceabaafa2d244839a506595ad55d1a3
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-01-26 23:11:43 +0700

    use transaction to create product

--- src/services/productImages.service.ts
+++ src/services/productImages.service.ts
@@ -1,12 +1,18 @@
 import format from "pg-format";
 import { pool } from "../db";
-export const addProductImages = async (images: Express.Multer.File[], productId: number) => {
-  console.log(typeof productId);
+import { PoolClient } from "pg";
+export const addProductImages = async (images: Express.Multer.File[], productId: number, client?: PoolClient) => {
   const imageValues = Object.values(images).map((image) => [image.filename, productId]);
 
   const query = format(`INSERT INTO product_images (image_url, product_id) VALUES %L RETURNING product_image_id`, imageValues);
 
-  const result = await pool.query(query);
+  let result;
+
+  if (client) {
+    result = await client.query(query);
+  } else {
+    result = await pool.query(query);
+  }
 
   return result.rows;
 };
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,24 +1,41 @@
 import { pool } from "../db";
+import { InternalServerError } from "../errors/InternalServerError";
 import { addProductImages } from "./productImages.service";
 
 export const addProduct = async (productData: { name: string; description: string; quantity: number; price: number }, displayImage: Express.Multer.File, productImages: Express.Multer.File[]) => {
-  const query = {
-    text: `
-        INSERT INTO products (name, description, quantity, price, display_price, display_image_url)
-        VALUES
-        ($1, $2, $3, $4, $5, $6)
-        RETURNING product_id;
-        `,
-    values: [productData.name, productData.description, productData.quantity, productData.price, productData.price, displayImage.filename],
-  };
-  const insertProductResult = await pool.query(query);
+  const client = await pool.connect();
 
-  const productId = insertProductResult.rows[0].product_id;
+  let productId, productImageIds;
+  try {
+    // start the transaction
+    await client.query("BEGIN");
 
-  const insertProductImageResult = await addProductImages(productImages, productId);
+    const query = {
+      text: `
+          INSERT INTO products (name, description, quantity, price, display_price, display_image_url)
+          VALUES
+          ($1, $2, $3, $4, $5, $6)
+          RETURNING product_id;
+          `,
+      values: [productData.name, productData.description, productData.quantity, productData.price, productData.price, displayImage.filename],
+    };
+    const insertProductResult = await client.query(query);
+
+    productId = insertProductResult.rows[0].product_id;
+
+    productImageIds = await addProductImages(productImages, productId, client);
+
+    // commit transaction
+    await client.query("COMMIT");
+  } catch (e) {
+    await client.query("ROLLBACK");
+    throw new InternalServerError("Error on creating product");
+  } finally {
+    client.release();
+  }
 
   return {
-    productId: insertProductResult.rows[0],
-    productImageIds: insertProductImageResult,
+    productId,
+    productImageIds,
   };
 };
