188628012b1e4b277bd37f30cfef68042c2441a5
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-02-01 21:48:59 +0700

    fix bug where empty array is passed as argument

--- src/services/productImage.service.ts
+++ src/services/productImage.service.ts
@@ -3,6 +3,10 @@ import { pool } from "../db";
 import { PoolClient } from "pg";
 
 export const addProductImages = async (images: Express.Multer.File[], productId: number, client?: PoolClient) => {
+  if (images.length === 0) {
+    return [];
+  }
+
   const imageValues = Object.values(images).map((image) => [image.path, productId]);
 
   const query = format(`INSERT INTO product_images (image_url, product_id) VALUES %L RETURNING product_image_id`, imageValues);
--- src/services/variantImage.service.ts
+++ src/services/variantImage.service.ts
@@ -12,10 +12,16 @@ export const addVariantImages = async (data: { variantId: number; variantImages:
   console.log(data);
   const imageValues: (string | number)[][] = [];
 
+  let isEmpty = true;
   for (const variant of data) {
     for (const variantImage of variant.variantImages) {
+      isEmpty = false;
       imageValues.push([variant.variantId, variantImage.path]);
     }
+  }
+
+  if (isEmpty) {
+    return [];
   }
 
   const query = format(`INSERT INTO variant_images (product_variant_id, image_url) VALUES %L RETURNING variant_image_id`, imageValues);
